e21311aae929184ab02ea536a5f83227a6d6bc92
AlexandreZajac <zajac.alexandre@gmail.com>
2019-08-25 10:33:59 +0200

    Badges readme

--- tests/test_decorpy.py
+++ tests/test_decorpy.py
@@ -6,7 +6,7 @@ from decorpy import timer, debug, check_types
 def test_types_count_input():
     """Test that exception is raised for invalid number of input parameters"""
 
-    @check_types(input=(int, int), output=(int, ))
+    @check_types(input=(int, int), output=(int,))
     def test(n):
         pass
 
@@ -18,7 +18,7 @@ def test_types_count_input():
 def test_types_count_output():
     """Test that exception is raised for invalid number of output values"""
 
-    @check_types(input=(int, ), output=(int, int))
+    @check_types(input=(int,), output=(int, int))
     def test(n):
         return n
 
@@ -30,11 +30,13 @@ def test_types_count_output():
 def test_types_check():
     """Test that exception is raised for invalid types"""
 
-    @check_types(input=(int, str), output=(str, ))
+    @check_types(input=(int, str), output=(str,))
     def test(n, base_str):
         return base_str * n
 
     with pytest.raises(Exception) as e:
         assert test("5", "test")
-    assert str(
-        e.value) == "The type of 5 and input[0] are different : <class 'str'> and <class 'int'>"
+    assert (
+        str(e.value)
+        == "The type of 5 and input[0] are different : <class 'str'> and <class 'int'>"
+    )
