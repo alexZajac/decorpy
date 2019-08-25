2dd8915959f59ff83a3c2f115fe77dcfbf61728e
AlexandreZajac <zajac.alexandre@gmail.com>
2019-08-25 12:17:13 +0200

    main release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/alexZajac/decorpy",
     packages=find_packages(),
-    download_url="https://github.com/alexZajac/decorpy/archive/v_01.tar.gz",
+    download_url="https://github.com/alexZajac/decorpy/archive/Main.tar.gz",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
